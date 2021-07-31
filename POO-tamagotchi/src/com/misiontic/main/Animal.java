41ebec6b1654dc336df36db670dee705ee39f9fc
Alexander suarez <alex.suarez@hotmail.com>
2021-07-30 21:42:13 -0500

    Avanze proyecto tamagotchi

--- POO-tamagotchi/src/com/misiontic/main/Animal.java
+++ POO-tamagotchi/src/com/misiontic/main/Animal.java
@@ -1,42 +1,68 @@
 package com.misiontic.main;
-
 import java.awt.Color;
-
 public class Animal {
-	int cantidadDePatas = 4;
-	Color colordePelo;
-	float peso;
-	static int felicidad;//va de 0 a 100, 0 es triste; 100 es feliz.
-	int salud;//va de 0 a 100, 0 muerto, 100 muy saludable.
-	int hambre;//va de 0 a 100, 0 es famelico y 100 es muy lleno.
-	
-	//Tengo que dar una respuesta cuando termine de comer?
-	//Yo quiero decir si ya termine de comer
-	private void comer() {
-		if(hambre >= 100) {
-			hambre = 100;
-			System.out.println("Estoy demasiado lleno, me voy a enfermar de tanto comer");
-			salud = salud -2;
-		}else {
-			if(hambre + 5 > 100) {
-				hambre = 100;
-			}else {
-				hambre = hambre +5;
-			}
-		}
-		
-	}
-	private void dormir() {
-		
-	}
-	private void irAlDoctor() {
-		
-	}
-	private void ejercitar() {
-		
-	}
-	private void jugar() {
-		
-	}
-
+    
+    double weight;
+    int happiness;//va de 0 a 100, 0 es triste; 100 es feliz.
+    int health;//va de 0 a 100, 0 muerto, 100 muy saludable.
+    int hungry;//va de 0 a 100, 0 es sin hambre y 100 es famelico.
+    int sleep; //va de 0 a 100, 0 es muy despierto y 100 es muy somnoliento
+    boolean isSleep;
+    
+    //Tengo que dar una respuesta cuando termine de comer?
+    //Yo quiero decir si ya termine de comer
+    private void eat() {
+        if(hungry <= 5) {
+        	hungry = 0;
+            System.out.println("Estoy demasiado lleno, me voy a enfermar de tanto comer");
+            health = health -2;
+            weight =weight+2;
+        }else {
+        	hungry = hungry - 5;
+        }
+    }
+    private void sleep() {
+    	if (sleep <=5 ) {
+    		sleep=0;
+    		System.out.println("Estoy inperactivo ");
+    		health = health -1;
+    		
+    	}
+    	else {
+    		sleep=sleep-10;
+    		weight=weight-1;
+    	}
+        
+    }
+    private void makeExercise() {
+    	if (sleep <40) {
+    		System.out.println("que comienze el ejercicio");
+    		health=health-3;
+    		weight=weight -0.5;
+    	}
+    	else {
+    		System.out.println("trata de descansar");	  
+    		sleep=sleep-3;
+    	}
+    }
+    private void play() {
+    	if (happiness< 30) {
+    		System.out.println("que empieze el juego");
+    		happiness =happiness-5;
+    	}
+    	else {
+    		System.out.println("");
+    	}
+    	
+        
+    }
+    private void gotoVeterinary() {
+    	health =health+2;
+    	hungry= hungry -1;
+    	
+    }
+    private void awake() {
+    	
+    }
+    
 }
--- POO-tamagotchi/src/com/misiontic/main/Main.java
+++ POO-tamagotchi/src/com/misiontic/main/Main.java
@@ -3,19 +3,21 @@ package com.misiontic.main;
 public class Main {
 
 	public static void main(String[] args) {
-		Animal perrito = new Animal();
-		perrito.hambre = 50;
-		perrito.felicidad = 50;
+		Animal cat = new Animal();
+		cat.hungry = 50;
+		cat.happiness = 50;
 		
+		/*
 		Animal gatico = new Animal();
-		gatico.hambre = 70;
-		gatico.felicidad = 70;
+		gatico.hungry = 70;
+		gatico.happiness = 70;
+		*/
 		
-		System.out.println("hambre perrito: " + perrito.hambre);
-		System.out.println("hambre gatico: " + gatico.hambre);
+		System.out.println("hambre perrito: " + cat.hungry);
+		//System.out.println("hambre gatico: " + gatico.hambre);
 		
-		System.out.println("felicidad perrito: " + perrito.felicidad);
-		System.out.println("felicidad gatico: " + gatico.felicidad);
+		//System.out.println("felicidad perrito: " + perrito.felicidad);
+		System.out.println("felicidad gatico: " + cat.happiness);
 
 	}
 
